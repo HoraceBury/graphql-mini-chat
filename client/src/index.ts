67895ee5f0960e5b438c7113888335f03487f8bf
Matthew Webster <horace.bury@gmail.com>
2026-04-27 13:30:19 +0200

    show mssg history on startup

--- client/src/index.ts
+++ client/src/index.ts
@@ -19,6 +19,25 @@ async function main() {
 
   console.log(`\nWelcome, ${username}! You can now start chatting.\n`)
 
+  // Fetch message history
+  try {
+    const historyResponse = await fetch('http://localhost:4000/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: 'query { messages { user content } }',
+      }),
+    })
+    const historyData: any = await historyResponse.json()
+    if (historyData.data?.messages) {
+      historyData.data.messages.forEach((msg: any) => {
+        console.log(`[${msg.user}]: ${msg.content}`)
+      })
+    }
+  } catch (err) {
+    console.error('Failed to fetch message history:', err)
+  }
+
   // Subscribe to messages
   client.subscribe(
     {
